f051b3b557493990f611fd8859c02df209bd1ed1
Michael van der Weg <michael@vanderweg.ch>
2013-06-19 17:09:54 +0200

    removed mutli instance bug

--- index.js
+++ index.js
@@ -3,34 +3,37 @@
 	// silly simple class
 	( function(){
 
+		// dont do this twice
+		if ( !Object.prototype.hasOwnProperty.call( String.prototype, "bold" ) ){
 		
-		// colorful strings
-		var stylize = function ( str, style ) {
-			var styles = {
-				  "bold": 		[ 1, 22 ]
-				, "italic": 	[ 3, 23 ]
-				, "underline": 	[ 4, 24 ]
-				, "inverse": 	[ 7, 27 ]
-				, "white": 		[ 37, 39 ]
-				, "grey": 		[ 90, 39 ]
-				, "black": 		[ 90, 39 ]
-				, "blue": 		[ 34, 39 ]
-				, "cyan": 		[ 36, 39 ]
-				, "green": 		[ 32, 39 ]
-				, "magenta": 	[ 35, 39 ]
-				, "red": 		[ 31, 39 ]
-				, "yellow": 	[ 33, 39 ]
+			// colorful strings
+			var stylize = function ( str, style ) {
+				var styles = {
+					  "bold": 		[ 1, 22 ]
+					, "italic": 	[ 3, 23 ]
+					, "underline": 	[ 4, 24 ]
+					, "inverse": 	[ 7, 27 ]
+					, "white": 		[ 37, 39 ]
+					, "grey": 		[ 90, 39 ]
+					, "black": 		[ 90, 39 ]
+					, "blue": 		[ 34, 39 ]
+					, "cyan": 		[ 36, 39 ]
+					, "green": 		[ 32, 39 ]
+					, "magenta": 	[ 35, 39 ]
+					, "red": 		[ 31, 39 ]
+					, "yellow": 	[ 33, 39 ]
+				};
+				return '\x1b[' + ( styles[ style ][ 0 ] ) + "m" + str + '\x1b[' + ( styles[ style ][ 1 ] ) + "m";
 			};
-			return '\x1b[' + ( styles[ style ][ 0 ] ) + "m" + str + '\x1b[' + ( styles[ style ][ 1 ] ) + "m";
-		};
-		
-		[ "bold", "underline", "italic", "inverse", "grey", "yellow", "red", "green", "blue", "white", "cyan", "magenta" ].forEach( function( style ) {
-			Object.defineProperty( String.prototype, style, {
-				get: function () {
-					return stylize(this, style);
-				}
+			
+			[ "bold", "underline", "italic", "inverse", "grey", "yellow", "red", "green", "blue", "white", "cyan", "magenta" ].forEach( function( style ) {
+				Object.defineProperty( String.prototype, style, {
+					get: function () {
+						return stylize(this, style);
+					}
+				} );
 			} );
-		} );
+		}
 
 
 		exports.init = function( scope ){}
