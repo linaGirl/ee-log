b4b66e23b8b966c42ca8311cf63e521e4da139b8
Michael van der Weg <michael@vanderweg.ch>
2013-06-20 16:30:37 +0200

    added gitignore && changed some variables

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-	"use strict";
+
+	var util = require( "util" );
 
 
-	( function(){
+	// colorful strings
+	var stylize = function ( str, style ) {
+		var styles = {
+			  "bold": 		[ 1, 22 ]
+			, "italic": 	[ 3, 23 ]
+			, "underline": 	[ 4, 24 ]
+			, "inverse": 	[ 7, 27 ]
+			, "white": 		[ 37, 39 ]
+			, "grey": 		[ 90, 39 ]
+			, "black": 		[ 90, 39 ]
+			, "blue": 		[ 34, 39 ]
+			, "cyan": 		[ 36, 39 ]
+			, "green": 		[ 32, 39 ]
+			, "magenta": 	[ 35, 39 ]
+			, "red": 		[ 31, 39 ]
+			, "yellow": 	[ 33, 39 ]
+		};
+		return '\x1b[' + ( styles[ style ][ 0 ] ) + "m" + str + '\x1b[' + ( styles[ style ][ 1 ] ) + "m";
+	};
+	
+	[ "bold", "underline", "italic", "inverse", "grey", "yellow", "red", "green", "blue", "white", "cyan", "magenta" ].forEach( function( style ) {
+		if ( !String[ style ] ){
+			Object.defineProperty( String.prototype, style, {
+				get: function () {
+					return stylize(this, style);
+				}
+			} );				
+		}
+	} );
 
 	
-		// colorful strings
-		var stylize = function ( str, style ) {
-			var styles = {
-				  "bold": 		[ 1, 22 ]
-				, "italic": 	[ 3, 23 ]
-				, "underline": 	[ 4, 24 ]
-				, "inverse": 	[ 7, 27 ]
-				, "white": 		[ 37, 39 ]
-				, "grey": 		[ 90, 39 ]
-				, "black": 		[ 90, 39 ]
-				, "blue": 		[ 34, 39 ]
-				, "cyan": 		[ 36, 39 ]
-				, "green": 		[ 32, 39 ]
-				, "magenta": 	[ 35, 39 ]
-				, "red": 		[ 31, 39 ]
-				, "yellow": 	[ 33, 39 ]
-			};
-			return '\x1b[' + ( styles[ style ][ 0 ] ) + "m" + str + '\x1b[' + ( styles[ style ][ 1 ] ) + "m";
-		};
-		
-		[ "bold", "underline", "italic", "inverse", "grey", "yellow", "red", "green", "blue", "white", "cyan", "magenta" ].forEach( function( style ) {
-			if ( !Object.prototype.hasOwnProperty.call( String.prototype, style ) ){
-				Object.defineProperty( String.prototype, style, {
-					get: function () {
-						return stylize(this, style);
-					}
-				} );				
-			}
-		} );
-	} )();
+
+
 
 
 
