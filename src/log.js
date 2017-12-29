0d76b1c2fad97b992dc39e7e81e6138397a94c75
Michael van der Weg <michael@joinbox.com>
2017-12-29 19:33:56 +0100

    feat: add new logd backend and remove all internal code

--- src/log.js
+++ src/log.js
@@ -1,42 +1,22 @@
-{
-    'use strict';
+
+'use strict';
 
 
-    const Logger        = require( "./Logger" );
-    const type          = require( "ee-types" );
+const Logger = require('./Logger');
+const logger = new Logger();
 
-
-
-    const logger = new Logger();
-
-
-    const log = function(...items) {
-        for (let i = 0, l = items.length; i <l; i++) {
-            let item = items[i];
-
-            if (type.error(item)) logger.trace(item, true);
-            else if (type.string(item)) {
-                let occurences = item.split(/\%s/gi).length - 1;
-                let list = items.slice(i+1, i+occurences+1);
-
-                list.unshift(item);
-                logger.log(list, 'white');
-
-                i += occurences;
-            }
-            else logger.log([item], 'white');
-        }
-    };
+const log = (...values) => {
+    logger.info(...values);
+};
 
 
 
-    ['trace', 'dir', 'wtf', 'info', 'debug', 'warn', 'error', 'highlight', 'success'].forEach((key) => {
-        if (!log[key]) log[key] = logger[key].bind(logger);
-        else throw new Error('Failed to map property «'+key+'» of logger to log :(');
-    });
+['trace', 'dir', 'wtf', 'info', 'debug', 'warn', 'error', 'highlight', 'success'].forEach((key) => {
+    log[key] = (...values) => {
+        logger[key](...values);
+    }
+});
 
 
 
-
-    module.exports = log;
-}
+module.exports = log;
--- test.js
+++ test.js
@@ -1,5 +1,12 @@
+var log = require( "./" );
 
-
-	var log = require( "./" );
-
-	log.info(1, null);
+log.debug(`im sitting in a plane from basel to london, it's shaking as fuck but programming is still fun!`);
+log.info(`im sitting in a plane from basel to london, it's shaking as fuck but programming is still fun!`);
+log.warn(`im sitting in a plane from basel to london, it's shaking as fuck but programming is still fun!`);
+log.error(`im sitting in a plane from basel to london, it's shaking as fuck but programming is still fun!`);
+log.highlight(`im sitting in a plane from basel to london, it's shaking as fuck but programming is still fun!`);
+log.success(`im sitting in a plane from basel to london, it's shaking as fuck but programming is still fun!`);
+log.wtf(`im sitting in a plane from basel to london, it's shaking as fuck but programming is still fun!`);
+log.dir(1,2,3, new Map());
+log.trace(new Error('not good!'));
+log('a string', 1338, new Date());
